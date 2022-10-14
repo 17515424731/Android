07e101f093b24ec113db44671b0a40ef2d776325
17515424731 <113672029+17515424731@users.noreply.github.com>
2022-10-14 16:50:28 +0800

    sy

---
+++ test/src/hello.java
@@ -0,0 +1,10 @@
+//表示public是hello的一个类，表示是一个公有的类
+//Hello{}表示一个类的开始和结束
+//public static void main(String[] args){}表示一个主方法 程序的入口 方法的开始和结束
+public class hello {
+    //编写一个main方法
+    public static void main(String[] args){
+        //表示输出Helloworld到屏幕上
+        System.out.println("Hello,world~");//；表示语句结束
+    }
+}
